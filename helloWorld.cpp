16433cc4ba488ff7e690a53b97df855fecb5a2a2
Sagee-dev <geethdesilvaws@gmail.com>
2025-03-20 20:04:01 +0100

    Create helloWorld.cpp

---
+++ helloWorld.cpp
@@ -0,0 +1,18 @@
+/*
+    Geeth De Silva
+    github : sagee-dev
+    basic helloword program for acedemic purpose
+*/
+ 
+//include input output library header
+#include <iostream>
+
+//Define main function code starts runing fro here
+int main(){
+
+    //output helloworld to the console
+    std::cout<<"Hello World";
+    
+    // return 0
+    return 0;
+}
